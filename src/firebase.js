4e78985feb5ca41e2a7db1d86a97b1d6b200f050
@oumar1994 <oumarfilston@gmail.com>
2023-04-10 16:40:06 -0700

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDnJwmshU7ERcKs9pu7qhyhieJ8TI1YSvo",
+  authDomain: "netflix-clone-94932.firebaseapp.com",
+  projectId: "netflix-clone-94932",
+  storageBucket: "netflix-clone-94932.appspot.com",
+  messagingSenderId: "183443726561",
+  appId: "1:183443726561:web:a14fe05bc30d23ba9897a0"
+};
+const app = initializeApp(firebaseConfig);
